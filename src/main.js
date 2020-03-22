e4a0cf65254961e4a24ccea659cbb10553a52171
Ozan Alpay <ozanalpay@yandex.com.tr>
2020-03-23 00:20:38 +0100

    Inkline Framework Added Into The Project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@inkline/inkline/dist/inkline.css";
+import Inkline from "@inkline/inkline";
+
+Vue.use(Inkline);
 
 Vue.config.productionTip = false;
 
