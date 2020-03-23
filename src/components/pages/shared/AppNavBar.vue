47b6d547420a151f072d11a84dd26f82ae123617
Ozan Alpay <ozanalpay@yandex.com.tr>
2020-03-23 01:04:13 +0100

    Some Initial Work Done

---
+++ src/components/pages/shared/AppNavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <i-layout>
+    <i-row>
+      <i-column xs="12" offset-xs="4">
+        <i-nav>
+          <i-nav-item href="#">COVID-19 Estimation</i-nav-item>
+          <i-nav-item>
+            <i-select v-model="selectedCountry" placeholder="Select A Country">
+              <i-select-option
+                v-for="(country, index) of countries"
+                :key="index"
+                :value="country"
+                :label="country"
+              />
+            </i-select>
+          </i-nav-item>
+          <!--<i-nav-item :to="{ name: 'index' }">Router Link</i-nav-item>
+          <i-nav-item>Item</i-nav-item>
+          <i-nav-item disabled>Disabled</i-nav-item> -->
+        </i-nav>
+      </i-column>
+    </i-row>
+    SelectedCountry is = {{ selectedCountry }}
+  </i-layout>
+</template>
+
+<script>
+export default {
+  name: "AppNavbar",
+  data() {
+    return {
+      countries: ["Turkey", "USA", "Germany", "Italy"],
+      selectedCountry: ""
+    };
+  }
+};
+</script>
+
+<style scoped>
+.i-nav {
+  margin-left: 10%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <i-layout>
+    <i-layout-header><AppNavBar /> </i-layout-header>
+    <i-layout vertical>
+      <i-layout-aside class="_background-primary">
+        Left Aside
+      </i-layout-aside>
+      <i-layout-content class="_background-secondary">
+        Right Aside
+      </i-layout-content>
+    </i-layout>
+  </i-layout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import AppNavBar from "@/components/pages/shared/AppNavBar.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    AppNavBar
   }
 };
 </script>
+<style scoped>
+.layout-aside {
+  width: 30% !important;
+}
+</style>
